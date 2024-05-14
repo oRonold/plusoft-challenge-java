081c83b1e04a6a0fb7ac4e04f23d4ad51bff3df0
oRonold <or040918@gmail.com>
2024-05-14 15:58:09 -0300

    Criando novas entidades conforme modelo

---
+++ src/main/java/br/com/fiap/challenge/sprint1/model/categoria/Categoria.java
@@ -0,0 +1,30 @@
+package br.com.fiap.challenge.sprint1.model.categoria;
+
+import br.com.fiap.challenge.sprint1.model.figuraPublica.FiguraPublica;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+
+@Entity
+@Table(name = "INOV_TB_CATEGORIA")
+@SequenceGenerator(name = "inov_categoria_seq", sequenceName = "inov_tb_categoria_seq", allocationSize = 1)
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "inov_categoria_seq")
+    @Column(name = "cd_categoria")
+    private Long codigo;
+    @Column(name = "nm_categoria", length = 50, nullable = false)
+    private String nome;
+
+    @OneToMany(mappedBy = "categoria")
+    private List<FiguraPublica> figuraPublica;
+
+}
---
+++ src/main/java/br/com/fiap/challenge/sprint1/model/score/Score.java
@@ -0,0 +1,32 @@
+package br.com.fiap.challenge.sprint1.model.score;
+
+import br.com.fiap.challenge.sprint1.model.figuraPublica.FiguraPublica;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+
+@Entity
+@Table(name = "INOV_TB_SCORE")
+@SequenceGenerator(name = "inov_score_seq", sequenceName = "inov_tb_score_seq", allocationSize = 1)
+public class Score {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "inov_score_seq")
+    @Column(name = "cd_score")
+    private Integer codigo;
+    @Column(name = "nr_score", nullable = false, scale = 2, precision = 10)
+    private BigDecimal numeroScore;
+
+    @OneToOne
+    @JoinColumn(name = "cd_fig_publica")
+    private FiguraPublica figuraPublica;
+
+
+}
