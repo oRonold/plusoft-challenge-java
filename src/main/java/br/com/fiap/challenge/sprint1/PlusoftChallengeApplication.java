1fc543ba12628dae63e3d14eec56382a0e8b22e7
oRonold <or040918@gmail.com>
2024-05-14 15:58:30 -0300

    Alterando configuracoes do projeto

--- src/main/java/br/com/fiap/challenge/sprint1/PlusoftChallengeApplication.java
+++ src/main/java/br/com/fiap/challenge/sprint1/PlusoftChallengeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Sprint1Application {
+public class PlusoftChallengeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Sprint1Application.class, args);
+		SpringApplication.run(PlusoftChallengeApplication.class, args);
 	}
 
 }
